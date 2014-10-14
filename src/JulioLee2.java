a93e07bc5cb06c1265ffdff41393245c7db41539
Victoria-Diaz <mavidiaz282@gmail.com>
2014-10-13 23:26:56 -0200

    Se cambió para que la clase principal maneje todo

--- src/JulioLee2.java
+++ src/JulioLee2.java
@@ -1,29 +1,121 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import lejos.nxt.ColorSensor;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
+import lejos.nxt.comm.NXTCommConnector;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.RS485;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class JulioLee2 {
 	private static SensorPort PUERTO_COLOR = SensorPort.S3;
-
+	private static int SIN_COMUNICACION = 0;
+	private static int GET_CONEXION = 1;
+	private static int SENSAR = 2;
+	private static int PATEAR = 3;
+	
 	public static void main(String[] args) {
 		// Inicializo el sensor
 		ColorSensor sensor = new ColorSensor(PUERTO_COLOR);
 		ClasificadorPelotas clasificador = new ClasificadorPelotas(sensor);
+		NXTCommConnector conector = RS485.getConnector();
+		NXTConnection conn;
+		DataInputStream dis;
+		DataOutputStream dos;
+		int lectura;
+		NXTRegulatedMotor motor_sensor = Motor.A;
+		motor_sensor.resetTachoCount();
+		motor_sensor.setSpeed(200);
+		motor_sensor.rotateTo(-4);
+		NXTRegulatedMotor motor_pateador = Motor.C;
+		motor_pateador.resetTachoCount();
 		
-		// Inicializacion de comportamientos
-		Behavior sensar = new Sensar(clasificador, Motor.A);
-		Behavior patear = new Patear(clasificador, Motor.A, Motor.C);
-		Behavior[] comportamientos = { sensar, patear};
-				
-		Arbitrator arbitro = new Arbitrator(comportamientos);
-		try {
-			Thread.sleep(1000);
-		} catch (Exception e) {
+		while(true){
+			conn = conector.waitForConnection(0, NXTConnection.PACKET);
+			Sound.twoBeeps();
+			dis = conn.openDataInputStream();
+			dos = conn.openDataOutputStream();
+
+			while (true) {
+				try {
+					lectura = dis.readInt();
+					
+					if(lectura == SENSAR){
+						// Se mueve el brazo del sensor a la posicion de sensar
+						motor_sensor.setSpeed(200);
+						motor_sensor.setAcceleration(3000);
+						motor_sensor.rotateTo(1);
+						
+						// Sensa y si es NADA acomoda el brazo para caminar y sigue
+						int color = clasificador.getColor();
+						try {
+							Thread.sleep(5000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						if(color == ClasificadorPelotas.NADA){
+							motor_sensor.rotateTo(-4);
+							// Termino la conexion
+							break;
+						}
+						
+						// Le devuelvo el color sensado si es azul o naranja
+						dos.writeInt(clasificador.getSensado());
+						dos.flush();
+					}else if(lectura == PATEAR){
+						// Acomodo el sensor
+						motor_sensor.setSpeed(300);
+						motor_sensor.rotateTo(-80);
+
+						// Acomodo el pateador
+						motor_pateador.setSpeed(100);
+						motor_pateador.rotateTo(-36);
+
+						// Pateo, si es naranja fuerte y si es azul despacio
+						if (clasificador.getSensado() == ClasificadorPelotas.NARANJA)
+							motor_pateador.setSpeed(900);
+
+						motor_pateador.rotateTo(20);
+
+						// Vuelvo el pateador a la posicion inical
+						motor_pateador.setSpeed(100);
+						motor_pateador.rotateTo(-2);
+
+						// Reseteo el sensor de color
+						clasificador.resetSensado();
+						
+						// Le aviso que termine
+						dos.writeInt(PATEAR);
+						dos.flush();
+					}else{
+						break;
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					break;
+				}
+			}
+			
+			try {
+				dis.close();
+				dos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				break;
+			}
+
+			conn.close();
+		
 		}
-		LCD.drawString("Arranca", 0, 0);
-		arbitro.start();
 	}
 }
